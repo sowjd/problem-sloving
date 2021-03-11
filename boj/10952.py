578322b1a1cf8895b524f916dc516332dc9c2fe2
Sojeong Sim <sowjd1225@gmail.com>
2021-03-11 22:20:11 +0900

    Fix when the input is 1 -1

--- boj/10952.py
+++ boj/10952.py
@@ -1,7 +1,14 @@
 import sys
 
 for line in sys.stdin:
-    result = sum(list(map(int, line.rstrip().split())))
-    if result == 0:
+    '''
+    문제의 조건: 0 < A, B < 10인 두 정수
+    원래 코드가 A = 1, B = -1일 때도 종료되서 수정함
+    '''
+    #result = sum(list(map(int, line.rstrip().split())))
+    a, b = map(int, line.rstrip().split())
+    
+    # if result == 0:
+    if a == 0 and b == 0:
         break
-    print(result)
+    print(a+b)
